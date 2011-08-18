ff673dae3b02c385f50f6cc12e2aef47e76d3d3b
Frederic Junod <frederic.junod@camptocamp.com>
2011-08-18 11:26:00 +0200

    new buildenv::python class (to replace python::dev)

---
+++ manifests/classes/python.pp
@@ -0,0 +1,11 @@
+class buildenv::python {
+
+  package {"python-dev":
+    ensure => present,
+    name   => $operatingsystem ? {
+      /RedHat|Fedora|CentOS/ => "python-devel",
+      /Debian|Ubuntu/        => "python-dev",
+    }
+  }
+
+}
